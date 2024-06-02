b074e602d14706d3104da19ee545a3b78893c3f4
Maria Irfan <mk4602086@gmail.com>
2024-06-02 23:24:19 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from"inquirer";
+const randomnumber = Math.floor(Math.random()*6+1);
+const answers= await inquirer.prompt([
+
+{   
+    name:"userGuessNumber",
+    type:"number",
+    message:"Please guess a number between 1-6",
+ }
+
+]);
+if(answers.userGuessNumber=== randomnumber){
+    console.log("congratulations!you guessed right number");
+
+}
+else{
+    console.log("you guessed wrong number");
+}
